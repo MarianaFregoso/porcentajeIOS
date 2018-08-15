a270508593a657df327c1dcc284f6c2ed61d4d4e
Maestro <Maestro@iMac-de-Maestro-3.local>
2018-08-14 18:44:06 -0700

    Se hicieron cambios

--- Propina/ViewController.swift
+++ Propina/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var txtdinero: UITextField!
+    @IBOutlet weak var porcentaje: UILabel!
+    @IBOutlet weak var procentajepropina: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func changetip(_ sender: UISlider) {
+        let currentvalue = Int(sender.value)
+        porcentaje.text = "\(currentvalue)"
+    }
+    
+        
+    
 }
 
